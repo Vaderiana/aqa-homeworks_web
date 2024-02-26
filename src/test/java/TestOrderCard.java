f6f9dae1a31839ac086b5c1e3ee7375792866de0
Vaderiana <120415111+Vaderiana@users.noreply.github.com>
2024-02-26 23:17:33 +0300

    uncomment

--- src/test/java/TestOrderCard.java
+++ src/test/java/TestOrderCard.java
@@ -23,7 +23,7 @@ public class TestOrderCard {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-//        options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:7777");
     }
